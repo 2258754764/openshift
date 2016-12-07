91b3c81e9d809af2d2fedc431e29dd9a12982510
Xiangtuoyang <xty19@users.noreply.github.com>
2016-12-07 22:40:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ header("Content-type:text/html;charset=utf-8");
 <br/>
 <a href="http://www.youtube.com">youtube</a>
 <br/>
+<a href="php_ping.php">ping</a>
+<br/>
 <a href="test1_mysql.php">test1_mysql</a>
 <br/>
 <a href="test1_mysql.php">test1_mysql</a>
